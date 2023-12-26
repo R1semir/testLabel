6d83ad0a56617d8b0c44cb5329ba068ca7767451
Emir <emirkan.ulker9@gmail.com>
2023-12-26 16:17:09 +0300

    testb

---
+++ testt/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int kalan = Convert.ToInt16(label2.Text);
+            kalan--;           
+            label2.Text = kalan.ToString();
+            if(kalan == 0)
+            {
+                textBox1.Enabled = false;
+            }
+        }
+    }
+}
